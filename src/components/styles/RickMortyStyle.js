29019296e8135a4d1c763fd2fa4c5e77d0255417
adalejc <adalejc@outlook.com>
2021-03-12 07:38:22 -0600

    feature(*):cursor pointer in card body

--- src/components/styles/RickMortyStyle.js
+++ src/components/styles/RickMortyStyle.js
@@ -25,6 +25,7 @@ export default css`
        margin: 1rem;
        position: relative;
        text-align: center;
+       cursor: pointer;
        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
        transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
      }
